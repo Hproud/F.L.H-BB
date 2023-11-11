25aae501fb02e1c730344a1f3d96ce9b410ff1af
Havin <havin.becca.93@gmail.com>
2023-11-11 17:41:41 -0600

    finished error handling auth me phase 2

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const {environment} = require('./config');
 const isProduction = environment === 'production';
+const {ValidationError} = require('sequelize');
 
 const app = express();
 
@@ -40,7 +41,36 @@ app.use (
 
         app.use(routes)
 
+        app.use((_erq, _res, next) =>{
+            const err = new Error("The requested resource couldn`t be found.");
+            err.title = "Resource  Not Found";
+            err.errors = {message: "The requested resource couldn`t be found."};
+            err.status = 404;
+            next(err)
+        });
 
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors){
+            errors[error.path] = error.message;
+        };
+        err.title = 'Validation error';
+        err.errors = errors
+    }
+    next(err)
+});
+
+app.use((err,_req,res,next) =>{
+    res.status(err.status || 500);
+    console.error(err);
+   res.json({
+        title: err.title || 'Server Error',
+message: err.message,
+errors: err.errors,
+stack: isProduction ? null : err.stack
+    });
+});
 
 
 module.exports = app;
