81ed9b69a4732293576d8793a28a37079f5f914e
Havin <havin.becca.93@gmail.com>
2023-11-14 23:56:06 -0600

    added backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -45,6 +45,21 @@ router.delete('/',(_req,res) => {
     return res.json({ message: 'success'})
 });
 
+router.get('/',
+async(req,res) =>{
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username
+        };
+        return res.json({
+            user: safeUser
+        })
+    }
+})
+
 
 
 module.exports = router;
