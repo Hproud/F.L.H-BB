205153a28ba186a2c47756309becaa8ef92c9285
Hproud <havin.becca.93@gmail.com>
2023-11-28 07:35:33 -0600

    commented out Op import'

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 const { handleValidationErrors}= require('../../utils/validation')
 const {setTokenCookie, requireAuth} = require('../../utils/auth');
 const {User} = require('../../db/models')
-const {Op} = require('sequelize')
+// const {Op} = require('sequelize')
 const router = express.Router();
 
 const validateSignUp = [
