b2622ca47ba7b1576213d75181868e1331902705
Hproud <havin.becca.93@gmail.com>
2023-12-05 21:59:25 -0600

    fixed association issue

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -18,8 +18,8 @@ module.exports = (sequelize, DataTypes) => {
 
 Booking.belongsTo(models.Spot,{
   foreignKey: 'spotId',
-  onDelete: 'CASCADE',
-  hooks: true
+  // onDelete: 'CASCADE',
+  // hooks: true
 })
 
 Booking.belongsTo(models.User,{
