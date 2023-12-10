14a3b8d472d2f4b5e2f942665f5e4eb9e96e85c8
Hproud <havin.becca.93@gmail.com>
2023-12-09 22:13:28 -0600

    review.js user mdl

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -48,17 +48,18 @@ Review.belongsTo(models.User,
     userId: {
       type: DataTypes.INTEGER,
       allowNull:false,
-      references: {
-        model: 'User',
+      // references: {
+      //   model: 'User',
       //   onDelete: 'CASCADE',
       //   hooks: true
-      }
+      // }
     },
     spotId: {
       type: DataTypes.INTEGER,
       allowNull:false,
       references:{
         model: 'Spots',
+
 
       }
     }
