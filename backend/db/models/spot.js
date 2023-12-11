76603aed9c02ad42693dba5bcda261d4f1511771
Hproud <havin.becca.93@gmail.com>
2023-12-10 19:52:13 -0600

    commented out seeders

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -71,7 +71,10 @@ Spot.belongsToMany(models.User,{
     Spot.init({
 ownerId:{
   type: DataTypes.INTEGER,
-
+references:{
+  model: 'Users',
+  key: 'id'
+}
   // allowNull: false
 },
     address: {
