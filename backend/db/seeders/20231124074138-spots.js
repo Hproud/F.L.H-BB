3236af920c48fb9730ac983dead4287a63e06651
Hproud <havin.becca.93@gmail.com>
2023-12-10 19:56:24 -0600

    commented out user seeders

--- backend/db/seeders/20231124074138-spots.js
+++ backend/db/seeders/20231124074138-spots.js
@@ -1,162 +1,162 @@
-'use strict';
-const {Spot} = require('../models');
+// 'use strict';
+// const {Spot} = require('../models');
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;
+// }
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
 
-    await Spot.bulkCreate([
-      {
-        ownerId: 1,
-        address: '13 cowboy lane',
-        city: 'Dallas',
-        state: 'Texas',
-        country: 'USA',
-        lat: 25.1548615,
-        lng: 90.1628379,
-        name: 'Test spot 1',
-        description: 'cozy little tent with a great fire',
-        price: 200,
-        previewImage: '/mytest/1'
-      },
-      {
-        ownerId: 3,
-        address: '30 Money lane',
-        city: 'Omaha',
-        state: 'Nebraska',
-        country: 'USA',
-        lat: 52.4865297,
-        lng: -3.4879331,
-        name:  'Test spot 2',
-        description: 'Under the ocean swimming with fish',
-        price: 20,
-        previewImage: '/mytest/2'
-      },
-      {
-        ownerId: 2,
-        address: '12 tell me lane',
-        city: 'Dallas',
-        state: 'Texas',
-        country: 'USA',
-        lat: 25.1548615,
-        lng: -150.1628379,
-        name: 'Test spot 3',
-        description: 'cozy little tent with a great fire',
-        price: 200,
-        previewImage: '/mytest/3'
-      },
-      {
-        ownerId: 3,
-        address: '120 tell me lane',
-        city: 'My City',
-        state: 'Ohio',
-        country: 'USA',
-        lat: 25.1872215,
-        lng: -179.1628379,
-        name: 'Test spot 4',
-        description: 'super soft couches',
-        price: 150,
-        previewImage: '/mytest/4'
-      },
-      {
-          ownerId: 1,
-        address: '158 test ave',
-        city: 'Dallas',
-        state: 'Texas',
-        country: 'USA',
-        lat: 25.1548615,
-        lng: -2.1797379,
-        name: 'Test spot 5',
-        description: 'the nicest castle you will see',
-        price: 1000,
-        previewImage: '/mytest/5'
-      }, {
-        ownerId: 4,
-        address: '22 Blinking St',
-        city: 'Nowhere',
-        state: 'Ohio',
-        country: 'USA',
-        lat: 89.1548615,
-        lng: -100.1628379,
-        name: 'Test spot 6',
-        description: 'Great place for kids!',
-        price: 80,
-        previewImage: '/mytest/6'
-      },
-      {
-        ownerId: 1,
-        address: '1001 Robot Ln',
-        city: 'memphis',
-        state: 'Tenn',
-        country: 'USA',
-        lat: 5.4865297,
-        lng: 120.4879331,
-        name:  'Test spot 7',
-        description: 'we have lots of places to BBQ',
-        price: 5,
-        previewImage: '/mytest/7'
-      },
-      {
-        ownerId: 3,
-        address: '808 wait up rd',
-        city: 'Tokyo',
-        state: 'Japan',
-        country: 'East Asia',
-        lat: 1.1548615,
-        lng: -179.1628379,
-        name: 'Test spot 8',
-        description: 'Great views and lots to do around the location!',
-        price: 350,
-        previewImage: '/mytest/8'
-      },
-      {
-        ownerId: 5,
-        address: '404 cant find me ave',
-        city: 'test City 1',
-        state: 'calgery',
-        country: 'canada',
-        lat: 75.1872215,
-        lng: -1.1628379,
-        name: 'Test spot 9',
-        description: 'We pride ourselves on giving you privacy',
-        price: 404,
-        previewImage: '/mytest/9'
-      },
-      {
-          ownerId: 3,
-        address: '12 candy lane',
-        city: 'Bluford',
-        state: 'Illinois',
-        country: 'USA',
-        lat: 55.1548615,
-        lng: -102.1797379,
-        name: 'Test spot 10',
-        description: 'We have absolute fine dining',
-        price: 2000,
-        previewImage: '/mytest/10'
-      },
+//     await Spot.bulkCreate([
+//       {
+//         ownerId: 1,
+//         address: '13 cowboy lane',
+//         city: 'Dallas',
+//         state: 'Texas',
+//         country: 'USA',
+//         lat: 25.1548615,
+//         lng: 90.1628379,
+//         name: 'Test spot 1',
+//         description: 'cozy little tent with a great fire',
+//         price: 200,
+//         previewImage: '/mytest/1'
+//       },
+//       {
+//         ownerId: 3,
+//         address: '30 Money lane',
+//         city: 'Omaha',
+//         state: 'Nebraska',
+//         country: 'USA',
+//         lat: 52.4865297,
+//         lng: -3.4879331,
+//         name:  'Test spot 2',
+//         description: 'Under the ocean swimming with fish',
+//         price: 20,
+//         previewImage: '/mytest/2'
+//       },
+//       {
+//         ownerId: 2,
+//         address: '12 tell me lane',
+//         city: 'Dallas',
+//         state: 'Texas',
+//         country: 'USA',
+//         lat: 25.1548615,
+//         lng: -150.1628379,
+//         name: 'Test spot 3',
+//         description: 'cozy little tent with a great fire',
+//         price: 200,
+//         previewImage: '/mytest/3'
+//       },
+//       {
+//         ownerId: 3,
+//         address: '120 tell me lane',
+//         city: 'My City',
+//         state: 'Ohio',
+//         country: 'USA',
+//         lat: 25.1872215,
+//         lng: -179.1628379,
+//         name: 'Test spot 4',
+//         description: 'super soft couches',
+//         price: 150,
+//         previewImage: '/mytest/4'
+//       },
+//       {
+//           ownerId: 1,
+//         address: '158 test ave',
+//         city: 'Dallas',
+//         state: 'Texas',
+//         country: 'USA',
+//         lat: 25.1548615,
+//         lng: -2.1797379,
+//         name: 'Test spot 5',
+//         description: 'the nicest castle you will see',
+//         price: 1000,
+//         previewImage: '/mytest/5'
+//       }, {
+//         ownerId: 4,
+//         address: '22 Blinking St',
+//         city: 'Nowhere',
+//         state: 'Ohio',
+//         country: 'USA',
+//         lat: 89.1548615,
+//         lng: -100.1628379,
+//         name: 'Test spot 6',
+//         description: 'Great place for kids!',
+//         price: 80,
+//         previewImage: '/mytest/6'
+//       },
+//       {
+//         ownerId: 1,
+//         address: '1001 Robot Ln',
+//         city: 'memphis',
+//         state: 'Tenn',
+//         country: 'USA',
+//         lat: 5.4865297,
+//         lng: 120.4879331,
+//         name:  'Test spot 7',
+//         description: 'we have lots of places to BBQ',
+//         price: 5,
+//         previewImage: '/mytest/7'
+//       },
+//       {
+//         ownerId: 3,
+//         address: '808 wait up rd',
+//         city: 'Tokyo',
+//         state: 'Japan',
+//         country: 'East Asia',
+//         lat: 1.1548615,
+//         lng: -179.1628379,
+//         name: 'Test spot 8',
+//         description: 'Great views and lots to do around the location!',
+//         price: 350,
+//         previewImage: '/mytest/8'
+//       },
+//       {
+//         ownerId: 5,
+//         address: '404 cant find me ave',
+//         city: 'test City 1',
+//         state: 'calgery',
+//         country: 'canada',
+//         lat: 75.1872215,
+//         lng: -1.1628379,
+//         name: 'Test spot 9',
+//         description: 'We pride ourselves on giving you privacy',
+//         price: 404,
+//         previewImage: '/mytest/9'
+//       },
+//       {
+//           ownerId: 3,
+//         address: '12 candy lane',
+//         city: 'Bluford',
+//         state: 'Illinois',
+//         country: 'USA',
+//         lat: 55.1548615,
+//         lng: -102.1797379,
+//         name: 'Test spot 10',
+//         description: 'We have absolute fine dining',
+//         price: 2000,
+//         previewImage: '/mytest/10'
+//       },
 
 
-    ],{validate: true })
-  },
+//     ],{validate: true })
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Spots';
-const Op = Sequelize.Op;
-    await queryInterface.bulkDelete(options, {
-      city: { [Op.in]: ['Dallas','Omaha','My City']}
-    },{});
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//     options.tableName = 'Spots';
+// const Op = Sequelize.Op;
+//     await queryInterface.bulkDelete(options, {
+//       city: { [Op.in]: ['Dallas','Omaha','My City']}
+//     },{});
+//   }
+// };
--- backend/db/seeders/20231124074150-reviews.js
+++ backend/db/seeders/20231124074150-reviews.js
@@ -1,109 +1,109 @@
-'use strict';
-const {Review} = require('../models');
+// 'use strict';
+// const {Review} = require('../models');
 
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;
+// }
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   try{
-   await Review.bulkCreate([
-    {
-      review: 'this was awesome',
-      stars: 5,
-      userId: 3,
-      spotId: 3,
-    },
-    {
-      review: 'well alright',
-      stars: 3,
-      userId: 1,
-      spotId: 2,
-    },
-    {
-      review: 'it was Ok',
-      stars: 3,
-      userId: 3,
-      spotId: 1,
-    },
-    {
-      review: 'the kids and wife loved it!',
-      stars: 5,
-      userId: 3,
-      spotId: 6,
-    },
-    {
-      review: 'staff were not very friendly but the room was nice',
-      stars: 2,
-      userId: 4,
-      spotId: 8,
-    },
-    {
-      review: 'it smelled like cows, too many farms around for my taste',
-      stars: 1,
-      userId: 2,
-      spotId: 10,
-    },
-    {
-      review: 'Loved all the cool gagets from the museum!',
-      stars: 4,
-      userId: 5,
-      spotId: 7,
-    },
-    {
-      review: 'there wasn`t much to do or see unfortunately',
-      stars: 1,
-      userId: 3,
-      spotId: 6,
-    },
-    {
-      review: 'absolutely LOVED IT!!!!',
-      stars: 5,
-      userId: 4,
-      spotId: 4,
-    },
-    {
-      review: 'This was a great family trip',
-      stars: 3,
-      userId: 1,
-      spotId: 9,
-    },
-    {
-      review: 'was located around everything we loved!',
-      stars: 4,
-      userId: 3,
-      spotId: 5,
-    },
-   ],{validate: true })
-  }catch(err){
-    console.log('this is my error', err)
-  }
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//    try{
+//    await Review.bulkCreate([
+//     {
+//       review: 'this was awesome',
+//       stars: 5,
+//       userId: 3,
+//       spotId: 3,
+//     },
+//     {
+//       review: 'well alright',
+//       stars: 3,
+//       userId: 1,
+//       spotId: 2,
+//     },
+//     {
+//       review: 'it was Ok',
+//       stars: 3,
+//       userId: 3,
+//       spotId: 1,
+//     },
+//     {
+//       review: 'the kids and wife loved it!',
+//       stars: 5,
+//       userId: 3,
+//       spotId: 6,
+//     },
+//     {
+//       review: 'staff were not very friendly but the room was nice',
+//       stars: 2,
+//       userId: 4,
+//       spotId: 8,
+//     },
+//     {
+//       review: 'it smelled like cows, too many farms around for my taste',
+//       stars: 1,
+//       userId: 2,
+//       spotId: 10,
+//     },
+//     {
+//       review: 'Loved all the cool gagets from the museum!',
+//       stars: 4,
+//       userId: 5,
+//       spotId: 7,
+//     },
+//     {
+//       review: 'there wasn`t much to do or see unfortunately',
+//       stars: 1,
+//       userId: 3,
+//       spotId: 6,
+//     },
+//     {
+//       review: 'absolutely LOVED IT!!!!',
+//       stars: 5,
+//       userId: 4,
+//       spotId: 4,
+//     },
+//     {
+//       review: 'This was a great family trip',
+//       stars: 3,
+//       userId: 1,
+//       spotId: 9,
+//     },
+//     {
+//       review: 'was located around everything we loved!',
+//       stars: 4,
+//       userId: 3,
+//       spotId: 5,
+//     },
+//    ],{validate: true })
+//   }catch(err){
+//     console.log('this is my error', err)
+//   }
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Reviews';
-const Op = Sequelize.Op;
-    await queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: ['3','2','1']}
-    },{});
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//     options.tableName = 'Reviews';
+// const Op = Sequelize.Op;
+//     await queryInterface.bulkDelete(options, {
+//       spotId: { [Op.in]: ['3','2','1']}
+//     },{});
+//   }
+// };
