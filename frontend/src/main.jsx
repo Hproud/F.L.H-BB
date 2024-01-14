f2a6251d01f50b13696d0995e012256bc7106289
Hproud <havin.becca.93@gmail.com>
2024-01-13 20:30:21 -0600

    fixed sign-in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+
+
 const store = configureStore();
 
 
