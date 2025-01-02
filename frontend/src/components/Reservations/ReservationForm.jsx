93b03698e7ff73341493fc5d66cdd902640baceb
Hproud <havin.becca.93@gmail.com>
2025-01-02 15:05:13 -0600

    added reservationForm component

---
+++ frontend/src/components/Reservations/ReservationForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ReservationForm() {
+  return
+   <div>
+<form>
+    
+</form>
+  </div>;
+}
--- frontend/src/components/Reservations/ReservationModal.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ReservationModal() {
-  return (
-    <div>ReservationModal</div>
-  )
-}
