fafbd70428493e5c32aedf3840a9bfbcd6fd5cf8
Hproud <havin.becca.93@gmail.com>
2025-01-02 16:15:57 -0600

    set guests state

--- frontend/src/components/Reservations/ReservationForm.jsx
+++ frontend/src/components/Reservations/ReservationForm.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React from 'react'
+import { useState } from 'react'
 
 export default function ReservationForm() {
-  return
-   <div>
+const [checkInDate,setCheckInDate] = useState()
+const [checkOutDate,setCheckOutDate] = useState()
+const [guests,setGuests]=useState(1)
+
+
+console.log(checkInDate)
+console.log(checkOutDate)
+
+
+
+return (
+    <div>
+<h1>Reserve This Location</h1>
 <form>
-    
+    <label>Check-in Date</label>
+    <br/>
+    <input
+    type='date'
+    onChange={(e) => setCheckInDate(e.target.value)}
+    />
+<br/>
+<br/>
+
+    <label>Check-Out Date</label>
+    <br/>
+    <input
+    type='date'
+    onChange={(e) => setCheckOutDate(e.target.value)}
+
+    />
+<br/>
+<br/>
+    <label>Guests </label>
+<input
+type='number'
+min={1}
+style={{width:'35px'}}
+
+/>
+<br/>
+<br/>
+<button>Reserve</button>
 </form>
-  </div>;
+        </div>
+  )
 }
